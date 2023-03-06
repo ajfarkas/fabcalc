f2e81db44832b926bf1566a0b8760bf4ce937d03
AJ Farkas <farkas.aj@gmail.com>
2023-03-06 16:13:22 -0500

    working converter

--- calc.js
+++ calc.js
@@ -11,17 +11,18 @@ const isWholeNumber = num => {
 	return parseInt(num) === parseFloat(num);
 };
 
-const decToFraction = (dec, precision = 3) => {
-	if (precision > 3) throw new Error('precision is too high. 3 places max.');
-
-	const rounded = parseFloat(dec.toFixed(precision));
-	const [whole, fract] = rounded.toString().split('.');
-	if (!fract) return whole;
-
-	const fractLen = fract.length;
-	let denominator = Math.pow(10, Math.min(fractLen, precision));
-	let numerator = fract;
-
+const reduceFraction = (numerator, denominator) => {
+	let whole = false;
+	// reduce to zero
+	if (numerator === 0) return '0';
+	// reduce to whole number
+	if (numerator % denominator === 0) {
+		return (numerator / denominator).toString();
+	}
+	// deal with mixed fractions
+	if (numerator > denominator) {
+		return decToFraction(numerator / denominator);
+	}
 	if (denominator % numerator === 0) {
 		denominator = denominator / numerator;
 		numerator = 1;
@@ -41,9 +42,52 @@ const decToFraction = (dec, precision = 3) => {
 		denominator = tempDenom;
 	}
 
-	const fraction = numerator === denominator ? '' : `${numerator}/${denominator}`;
+	return numerator === denominator ? '' : `${numerator}/${denominator}`;
+};
+
+const decToFraction = (dec, precision = 3) => {
+	if (precision > 3) throw new Error('precision is too high. 3 places max.');
+
+	const rounded = parseFloat(dec.toFixed(precision));
+	const [whole, fract] = rounded.toString().split('.');
+	if (!fract) return whole;
+
+	const fractLen = fract.length;
+	let denominator = Math.pow(10, Math.min(fractLen, precision));
+	let numerator = fract;
+
+	const fraction = reduceFraction(numerator, denominator);
 	return whole !== '0'
 		? `${whole} ${fraction}`
 		: fraction;
 };
 
+const roundToDenominator = (num, denominator, round = false) => {
+	if (typeof denominator !== 'number') throw new Error('denominator arg must be of type number.');
+	if (denominator === 0) throw new Error('cannot divide by 0');
+	let numerator;
+
+	const [numer, denom] = num.split('/').map(n => parseInt(n));
+	const reductionFactor = denom / denominator;
+	const reducedNumerator = numer / reductionFactor;
+	if (round === 'down') {
+		numerator = Math.floor(reducedNumerator);
+	} else if (round === 'up') {
+		numerator = Math.ceil(reducedNumerator);
+	} else {
+		numerator = Math.round(reducedNumerator);
+	}
+
+	return reduceFraction(numerator, denominator);
+};
+
+const decToRoundedFraction = (dec, precision, denominator, round) => {
+	const mixed = decToFraction(dec, precision);
+	const [whole, fraction] = mixed.split(' ');
+
+	if (fraction) {
+		const fractString = roundToDenominator(fraction, denominator, round);
+		return fractString !== '0' ? `${whole} ${fractString}` : whole;
+	}
+	return roundToDenominator(whole, denominator, round);
+};
--- test/calc.test.js
+++ test/calc.test.js
@@ -41,6 +41,28 @@ runTests(() => {
 			expect(isWholeNumber(NaN)).toEqual(false);
 		});
 
+		describe('Reduce fraction', () => {
+			it('should reduce any multiple', () => {
+				expect(reduceFraction(2, 4)).toEqual('1/2');
+				expect(reduceFraction(8, 16)).toEqual('1/2');
+				expect(reduceFraction(3, 6)).toEqual('1/2');
+			});
+
+			it('should reduce to 0', () => {
+				expect(reduceFraction(0, 4)).toEqual('0');
+			});
+
+			it('should reduce to whole numbers', () => {
+				expect(reduceFraction(3, 3)).toEqual('1');
+				expect(reduceFraction(9, 3)).toEqual('3');
+			});
+
+			it('should handle mixed fractions', () => {
+				expect(reduceFraction(3, 2)).toEqual('1 1/2');
+				expect(reduceFraction(18, 16)).toEqual('1 1/8');
+			});
+		});
+
 		describe('Convert decimal to fraction', () => {
 			it('should work with whole numbers', () => {
 				expect(decToFraction(25)).toEqual('25');
@@ -76,7 +98,54 @@ runTests(() => {
 					if (err.toString() !== "Error: precision is too high. 3 places max.")
 					throw new Error('Expected to receive an error.');
 				}
-			})
-		})
+			});
+		});
+
+		describe('Round to Denominator', () => {
+			it('should round to the nearest fraction of the given denominator', () => {
+				expect(roundToDenominator('3/50', 16)).toEqual('1/16');
+				expect(roundToDenominator('19/50', 8)).toEqual('3/8');
+				expect(roundToDenominator('119/50', 8)).toEqual('2 3/8');
+				expect(roundToDenominator('99/100', 32)).toEqual('1');
+				expect(roundToDenominator('1/100', 32)).toEqual('0');
+			});
+
+			it('should round and reduce', () => {
+				expect(roundToDenominator('33/64', 16)).toEqual('1/2');
+			});
+
+			it('should not divide by zero', () => {
+				try {
+					expect(roundToDenominator('1/2', 0)).toEqual('0');
+				} catch(err) {
+					if (err.toString() !== 'Error: cannot divide by 0')
+					throw new Error('Expected to receive an error.');
+				}
+			});
+
+			it('should round down to nearest fraction of the given denominator', () => {
+				expect(roundToDenominator('4/50', 16, 'down')).toEqual('1/16');
+				expect(roundToDenominator('19/50', 8, 'down')).toEqual('3/8');
+				expect(roundToDenominator('69/50', 8, 'down')).toEqual('1 3/8');
+			});
+
+			it('should round up to the nearest fraction of the given denominator', () => {
+				expect(roundToDenominator('13/50', 8, 'up')).toEqual('3/8');
+				expect(roundToDenominator('1/3', 1, 'up')).toEqual('1');
+				expect(roundToDenominator('5/4', 2, 'up')).toEqual('1 1/2');
+			});
+		});
+
+		describe('Convert decimal to rounded fraction', () => {
+			it('should round the decimal to the nearest given denominator', () => {
+				expect(decToRoundedFraction(2.1, 1, 8, false)).toEqual('2 1/8');
+				expect(decToRoundedFraction(0.02, 2, 16, false)).toEqual('0');
+			});
+
+			it('should round the decimal up or down to the nearest given denominator', () => {
+				expect(decToRoundedFraction(1.2, 1, 3, 'down')).toEqual('1');
+				expect(decToRoundedFraction(1.01, 2, 3, 'up')).toEqual('1 1/3');
+			});
+		});
 	});
 });
